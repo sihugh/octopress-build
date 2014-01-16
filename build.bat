2ee74b899876d5accc083851e5b863f1f5fa2e06
Simon <si@hughesdon.com>
2014-01-16 21:35:31 +0000

    First commit

---
+++ build.bat
@@ -0,0 +1,16 @@
+set GIT_CMD=git.exe
+
+%GIT_CMD% clone https://github.com/sihugh/octopress
+%GIT_CMD% clone https://github.com/sihugh/darkstripes .\octopress\.themes\darkstripes
+
+pushd octopress
+
+call gem install bundler
+call bundle install
+call bundle exec rake install['darkstripes']
+
+%GIT_CMD% clone https://github.com/sihugh/mdf  source\_posts
+
+call bundle exec rake generate
+
+popd
